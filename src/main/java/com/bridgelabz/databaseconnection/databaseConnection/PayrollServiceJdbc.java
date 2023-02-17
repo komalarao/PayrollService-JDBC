d85d8db569a8e90bd1baeb4ab87f42257610efc1
komalarao <manoharvakamullu1999@gmail.com>
2023-02-17 01:52:45 -0800

    added: program for create jdbc connection to sql

---
+++ src/main/java/com/bridgelabz/databaseconnection/databaseConnection/PayrollServiceJdbc.java
@@ -0,0 +1,25 @@
+package com.bridgelabz.databaseconnection.databaseConnection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class PayrollServiceJdbc {
+	public static void main(String args[]) {
+		try {
+			Connection connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/payrollservice", "root",
+					"root");
+			Statement statement = connect.createStatement();
+
+			if (statement != null) {
+				System.out.println("connection is successful:");
+			}
+			ResultSet output = statement.executeQuery("select * from employee_payroll ;");
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
